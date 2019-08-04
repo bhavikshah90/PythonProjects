6f8f817b8fdc56bcfa34b73d1d790ac32974eb0a
bhavikshah90 <34947774+bhavikshah90@users.noreply.github.com>
2019-08-04 14:25:19 -0400

    CSV ceation for start and end time
    Track start time and end time of object using live video webcam capture

--- MotionCapture/motion_capture.py
+++ MotionCapture/motion_capture.py
@@ -1,7 +1,13 @@
-import cv2, time
+import cv2, time, pandas
+from datetime import datetime
+
 first_frame = None # variable to store first frame to capture start time
 video = cv2.VideoCapture(0)
+status_list = [None,None]
+time_now = []
+df = pandas.DataFrame(columns = ["Start","End"])
 while True:
+
     check,frame = video.read()
     status = 0
 
@@ -15,17 +21,21 @@ while True:
     threshold_frame = cv2.threshold(diff_frame,30,255,cv2.THRESH_BINARY)[1]
     threshold_frame = cv2.dilate(threshold_frame,None,iterations = 2)
 
-    (_,cnts,_)=cv2.findContours(threshold_frame.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    cnts, hierachy=cv2.findContours(threshold_frame.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in cnts:
-        if cv2.contourArea(contour) < 1000:
+        if cv2.contourArea(contour) < 10000:
             continue
-
+        status = 1
         (x,y,w,h) = cv2.boundingRect(contour)
         cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
 
+    status_list.append(status)
 
-
+    if status_list[-1] == 1 and status_list[-2] == 0:
+        time_now.append(datetime.now())
+    if status_list[-1] == 0 and status_list[-2] == 1:
+        time_now.append(datetime.now())
     cv2.imshow("currently capturing ",gray_img)
     cv2.imshow("diff_frame",diff_frame)
     cv2.imshow("threshold", threshold_frame)
@@ -33,7 +43,13 @@ while True:
 
     key = cv2.waitKey(1)
     if key == ord('q'):
+        if status == 1:
+            time_now.append(datetime.now())
         break
-
+# print(time_now)
+for times in range(0,len(time_now),2):
+    df=df.append({"Start":time_now[times],"End":time_now[times+1]},ignore_index=True)
+df.to_csv("TimeTracker.csv")
+print(df)
 video.release()
 cv2.destroyAllWindows()
