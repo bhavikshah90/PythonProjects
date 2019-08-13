e3643e6fe644d07a030305e3843e6a78d4957412
bhavikshah90 <34947774+bhavikshah90@users.noreply.github.com>
2019-08-13 12:33:25 -0400

    sample images addition
    Added sample images

--- face_detection/face_detection.py
+++ face_detection/face_detection.py
@@ -1,7 +1,7 @@
 import cv2
 import glob
 
-images = glob.glob("*.jpeg")
+images = glob.glob("*.jpg")
 print(images)
 
 
@@ -10,15 +10,17 @@ for i in images:
     img = cv2.imread(i)
 
     gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    resized_img = cv2.resize(gray_img,(int(gray_img.shape[1]/3),int(gray_img.shape[0]/3)))
-    faces = face_cascade.detectMultiScale(resized_img,
-                                          scaleFactor=1.05,
-                                          minNeighbors = 5)
+    # resized_img = cv2.resize(gray_img,(int(img.shape[1]/3),int(img.shape[0]/3)))
+    faces = face_cascade.detectMultiScale(gray_img,
+                                          scaleFactor=1.04,
+                                          minNeighbors=2,
+                                          flags=cv2.CASCADE_SCALE_IMAGE
+                                          )
 
     print (type(faces))
     print (faces)
     for x,y,w,h in faces:
-        img = cv2.rectangle(resized_img,(x,y),(x+w,y+h),(0,255,0),3)
+        img = cv2.rectangle(gray_img,(x,y),(x+w,y+h),(0,255,0),2)
     # resized_output_img = cv2.resize(img,(int(img.shape[1]/3),int(img.shape[0]/3)))
     cv2.imshow("updated_img",img)
     cv2.waitKey(0)
--- face_detection/image_resize.py
+++ face_detection/image_resize.py
@@ -8,6 +8,6 @@ print(img.ndim)
 
 resized_img = cv2.resize(img,(200,500))
 cv2.imshow("minions",resized_img)
-cv2.imwrite("minions.resized.jpg",resized_img)
+cv2.imwrite("minions1.resized.jpg",resized_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
